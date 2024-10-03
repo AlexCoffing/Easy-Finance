ff3a57b63bfe9fff7ef7c9bd75abc81bf3066c96
Alexander Dev <brandondelapaz@hotmail.com>
2024-10-03 01:28:36 -0600

    AsesorBot 0.1.1
    Mejoras en las respuestas

--- training.py
+++ training.py
@@ -80,5 +80,5 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = gradient_descent_v2.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', train_process)
