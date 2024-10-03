24c44ff2bcc2608857dd1dfe2adf5a0d7cbdd845
Alexander Dev <brandondelapaz@hotmail.com>
2024-10-03 00:24:25 -0600

    Modelo
    Se realiza el modelo del chatbot

--- training.py
+++ training.py
@@ -6,6 +6,16 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+from tensorflow.python.keras.engine import data_adapter
+
+def _is_distributed_dataset(ds):
+    return isinstance(ds, data_adapter.input_lib.DistributedDatasetSpec)
+data_adapter._is_distributed_dataset = _is_distributed_dataset
+
+import tensorflow.python.keras as tf_keras
+from keras import __version__
+tf_keras.__version__ = __version__
+
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, Activation
 from tensorflow.python.keras.optimizers import gradient_descent_v2
@@ -56,3 +66,19 @@ random.shuffle(training)
 training = np.array(training, dtype="object")
 
 print(training)
+
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = gradient_descent_v2.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', train_process)
